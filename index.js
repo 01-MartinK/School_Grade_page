73b078cce52621010b225317e428b65b5961b229
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-04-04 10:24:45 +0300

    As an unauthenticated user I can login [Delivers #181767764]

--- index.js
+++ index.js
@@ -1,8 +1,95 @@
 // prequisitions
 const express = require('express');
-const path = require('path');
+const cors = require('cors')
+// const path = require('path');
 const app = express();
+app.use(cors())        // Avoid CORS errors in browsers
+app.use(express.json()) // Populate req.body
+const users = [
+    {id: 1, username: "Admin", password: "Password", isAdmin: true},
+    {id: 2, username: "User", password: "Password", isAdmin: false}
+]
 
+let sessions = []
+// Function to check that the user is an admin
+/*
+function requireAdmin(req, res, next) {
+    // Check that the sessionId is present
+    if (!req.body.sessionId) {
+        return res.status(400).send({error: 'You have to login'})
+    }
+
+    // Check that the sessionId is valid
+    const sessionUser = sessions.find((session) => session.id === parseInt(req.body.sessionId));
+    if (!sessionUser) {
+        return res.status(401).send({error: 'Invalid sessionId'})
+    }
+
+    // Check that the sessionId in the sessions has user in it
+    const user = users.findById(sessionUser.userId);
+    if (!user) {
+        return res.status(400).send({error: 'SessionId does not have an user associated with it'})
+    }
+
+    // Check that the user is an admin
+    if (!user.isAdmin) {
+        return res.status(400).send({error: 'Insufficient permissions'})
+    }
+    next()
+}
+*/
+Array.prototype.findById = function (value) {
+    return this.findBy('id', parseInt(value))
+}
+Array.prototype.findBy = function (field, value) {
+    return this.find(function (x) {
+        return x[field] === value;
+    })
+}
+app.get('/users', (req, res) => {
+    res.status(200).send(users)
+})
+
+app.post('/users', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+
+    let user = users.findBy('username', req.body.username);
+    if (user) {
+        return res.status(409).send({error: 'Conflict: The user already exists. '})
+    }
+    users.push({id: users.length + 1, username: req.body.username, password: req.body.password, isAdmin: false})
+
+    user = users.findById(users.length);
+    let newSession = {
+        id: sessions.length + 1,
+        userId: user.id
+    }
+    sessions.push(newSession)
+    res.status(201).send({sessionId: sessions.length})
+})
+app.post('/sessions', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+    const user = users.find((user) => user.username === req.body.username && user.password === req.body.password);
+    if (!user) {
+        return res.status(401).send({error: 'Unauthorized: username or password is incorrect'})
+    }
+    let newSession = {
+        id: sessions.length + 1,
+        userId: user.id
+    }
+    sessions.push(newSession)
+    res.status(201).send(
+        {sessionId: sessions.length}
+    )
+})
+app.delete('/sessions', (req, res) => {
+    sessions = sessions.filter((session) => session.id === req.body.sessionId);
+    res.status(200).end()
+})
 // bodyParser Use
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
