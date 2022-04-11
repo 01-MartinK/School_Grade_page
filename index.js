2deeb5ed0be5381b40d5627f9ac85a04c8e937e7
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-04-11 15:53:55 +0300

    HOTFIX A school can see the students, teachers, subjects and Fail students (Ranking board fixes ignore, its WIP). [Delivers #181767377]

--- index.js
+++ index.js
@@ -1,8 +1,136 @@
 // prequisitions
 const express = require('express');
-const path = require('path');
+const cors = require('cors')
+// const path = require('path');
 const app = express();
+app.use(cors())        // Avoid CORS errors in browsers
+app.use(express.json()) // Populate req.body
+const users = [
+    {personalId: '0', name: "Admin Teacher", password: "Pass", isAdmin: true, email: 'Admin', school: 'Admin School'},
+    {personalId: '1', name: "Kevin Hartman", password: "Pass", isAdmin: false, email: 'User', school: 'User School'},
+    {personalId: '2', name: "Teacher", password: "Pass", isAdmin: true, email: 'Teacher', school: 'Teacher School'}
+]
 
+let sessions = [
+    {
+        userId: '0',
+        id: 'Admin'
+    },
+    {
+        userId: '1',
+        id: 'User'
+    },
+    {
+        userId: '2',
+        id: 'Teacher'
+    }
+]
+
+
+
+const temp_grades = [
+    { personalId: '1', subject: "Mathematics", grades: [3, 2, 4, 3, 4, 4, 3, 5, 4] },
+    { personalId: '1', subject: "Matemaatika", grades: [3, 2, 4, 3, 4, 4, 3, 5, 4] },
+    { personalId: '1', subject: "Drugate", grades: [4, 4, 5, 5, 5, 5, 5] },
+    { personalId: '1', subject: "Keemia", grades: [4, 4, 5, 5, 5, 5, 5] },
+    { personalId: '1', subject: "Biology", grades: [4, 5, 4, 5, 5, 5, 3, 4, 5] },
+    { personalId: '1', subject: "Bioloogia", grades: [4, 5, 4, 5, 5, 5, 3, 4, 5] },
+]
+
+const temp_classes = [
+    { name: "Proga", time: "14.00-16.00", subjects: ["math", "programming"], teacher: "Margit Mägi" },
+    { name: "Matta", time: "17.00-18.00", subjects: ["trigonometry", "simple equations"], teacher: "Anne mootse" },
+]
+
+const temp_letters = [
+    { text: "lorem jasopidjasopidj aspod jasüp djaspodj aspo djpaosjd poasjd poaj dpojasp odjas d", teacher: "Margit Mägi", class: "Proga" },
+    { text: "lorem jasopidjasopidj aspod jasüp djaspodj aspo djpaosjd poasjd poaj dpojasp odjas d", teacher: "Anne Mootse", class: "Mathematics" },
+]
+
+const temp_subjects = [
+    { name: "Keemia", progress: "läbitud" },
+    { name: "Matemaatika", progress: "pooleli" },
+    { name: "Eesti keel", progress: "läbikukkunud" }
+]
+
+const temp_scores = [
+    { text: "Enne töö, siis lõbu.", student: "Kevin Hartman"},
+    { text: "Vana karu tantsima ei õpi.", student: "Uno Erik"},
+]
+
+// Function to check that the user is an admin
+function requireAdmin(req, res, next) {
+    // Check that the sessionId is present
+    if (!req.body.sessionId) {
+        return res.status(400).send({error: 'You have to login'})
+    }
+    // Check that the sessionId is valid
+    const sessionUser = sessions.findBy('userId', req.body.sessionId);
+    if (!sessionUser) {
+        return res.status(401).send({error: 'Invalid sessionId'})
+    }
+    // Check that the sessionId in the sessions has user in it
+    const user = users.findBy('personalId', sessionUser.userId);
+    if (!user) {
+        return res.status(400).send({error: 'SessionId does not have an user associated with it'})
+    }
+    // Check that the user is an admin
+    if (!user.isAdmin) {
+        return res.status(400).send({error: 'Insufficient permissions'})
+    }
+    next()
+}
+Array.prototype.findById = function (value) {
+    return this.findBy('id', parseInt(value))
+}
+Array.prototype.findBy = function (field, value) {
+    return this.find(function (x) {
+        return x[field] === value;
+    })
+}
+
+app.post('/users', (req, res) => {
+    if (!req.body.name || !req.body.password || !req.body.personalId || !req.body.email || !req.body.school) {
+        return res.status(400).send({error: 'One or multiple params are missing'})
+    }
+    let personalId = users.findBy('personalId', req.body.personalId);
+    if (personalId) {
+        return res.status(409).send({error: 'Conflict: The personal Id already exists, you have already made an account with this id. Contact support if you think you did not create an account. '})
+    }
+    let email = users.findBy('email', req.body.email);
+    if (email) {
+        return res.status(409).send({error: 'Conflict: The email already exists. Press Forgot password to send account details to the email.'})
+    }
+    users.push({personalId: req.body.personalId, name: req.body.name, password: req.body.password, isAdmin: false, email: req.body.email, school: req.body.school})
+    user = users.findBy('personalId', req.body.personalId);
+    let newSession = {
+        userId: user.personalId,
+        id: req.body.email
+    }
+    sessions.push(newSession)
+    res.status(201).send({sessionId: sessions.findBy('id', req.body.email).userId })
+})
+app.post('/sessions', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+    const user = users.find((user) => user.email === req.body.email && user.password === req.body.password);
+    if (!user) {
+        return res.status(401).send({error: 'Unauthorized: email or password is incorrect'})
+    }
+    let newSession = {
+        userId: user.personalId,
+        id: req.body.email
+    }
+    sessions.push(newSession)
+    res.status(201).send(
+        {sessionId: sessions.findBy('id', req.body.email).userId}
+    )
+})
+app.delete('/sessions', (req, res) => {
+    sessions = sessions.filter((session) => session.userId === req.body.sessionId);
+    res.status(200).end()
+})
 // bodyParser Use
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,19 +142,56 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'))
 
 // render default page
-app.get('/student/kutseope', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
+// render school page
+app.get('/student/kutseope', (req, res) => {
+    res.render('schoolPage');
+});
 app.get('/student/info', (req, res) => {
     res.render('student_info');
 });
+app.post('/API/GRADES', (req, res) => {
+    let temp_grades2 = temp_grades.filter((grade) => grade.personalId === (req.body.sessionId));
+    res.send(temp_grades2)
+})
+app.get('/API/ALLGRADES', (req, res) => {
+    res.send(temp_grades)
+})
+app.get('/API/SCHEDULE', (req, res) => {
+    res.send(temp_classes)
+})
 
-const APIRouter = require('./routers/api');
-
-app.use('/API', APIRouter);
-
-
+app.get('/API/LETTERS', (req, res) => {
+    res.send(temp_letters)
+})
+app.get('/API/SUBJECT_BY_USER', (req, res) => {
+    res.send(temp_subjects)
+})
+app.get('/API/LEADERBOARD', (req, res) => {
+    res.send(temp_scores)
+})
+app.post('/API/ADMIN/STUDENTS', requireAdmin, (req, res) => {
+    let temp_admin = users.filter((user) => user.isAdmin === false);
+    res.send(temp_admin)
+})
+app.delete('/API/ADMIN/STUDENTS', requireAdmin, (req, res) => {
+    times = times.filter((time) => time.id !== req.body.name);
+    let temp_admin = users.filter((user) => user.isAdmin === false && user.name !== req.body.name);
+    res.send(temp_admin)
+})
+app.post('/API/ADMIN/TEACHERS', requireAdmin, (req, res) => {
+    let temp_admin = users.filter((user) => user.isAdmin === true);
+    res.send(temp_admin)
+})
+app.post('/API/ADMIN/SUBJECTS', requireAdmin, (req, res) => {
+    res.send(temp_subjects)
+})
+app.get('/admin', (req, res) => {
+    res.render('admin');
+});
 const server = app.listen(3010, () => {
     console.log(`Express running -> PORT ${server.address().port}`)
 });
