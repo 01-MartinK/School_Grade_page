501ec3f26be186c115d63d3384bf028368fdfe01
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2022-04-19 11:33:55 +0300

    Fixes to index.js file

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // prequisitions
 const express = require('express');
 const cors = require('cors')
-// const path = require('path');
+    // const path = require('path');
 const app = express();
-app.use(cors())        // Avoid CORS errors in browsers
+app.use(cors()) // Avoid CORS errors in browsers
 app.use(express.json()) // Populate req.body
 let users = [
-    {personalId: '0', name: "Admin Teacher", password: "Pass", isAdmin: true, email: 'Admin', school: 'Admin School'},
-    {personalId: '1', name: "Kevin Hartman", password: "Pass", isAdmin: false, email: 'User', school: 'User School'},
-    {personalId: '2', name: "Teacher", password: "Pass", isAdmin: true, email: 'Teacher', school: 'Teacher School'}
+    { personalId: '0', name: "Admin Teacher", password: "Pass", isAdmin: true, email: 'Admin', school: 'Admin School' },
+    { personalId: '1', name: "Kevin Hartman", password: "Pass", isAdmin: false, email: 'User', school: 'User School' },
+    { personalId: '2', name: "Teacher", password: "Pass", isAdmin: true, email: 'Teacher', school: 'Teacher School' }
 ]
 
-let sessions = [
-    {
+let sessions = [{
         userId: '0',
         id: 'Admin'
     },
@@ -26,40 +25,37 @@ let sessions = [
     }
 ]
 
-const temp_scores = [
-    { text: "Enne töö, siis lõbu.", student: "Kevin Hartman"},
-    { text: "Vana karu tantsima ei õpi.", student: "Uno Erik"},
-]
-
 // Function to check that the user is an admin
 function requireAdmin(req, res, next) {
     // Check that the sessionId is present
     if (!req.body.sessionId) {
-        return res.status(400).send({error: 'You have to login'})
+        return res.status(400).send({ error: 'You have to login' })
     }
     // Check that the sessionId is valid
     const sessionUser = sessions.findBy('userId', req.body.sessionId);
     if (!sessionUser) {
-        return res.status(401).send({error: 'Invalid sessionId'})
+        return res.status(401).send({ error: 'Invalid sessionId' })
     }
     // Check that the sessionId in the sessions has user in it
     const user = users.findBy('personalId', sessionUser.userId);
     if (!user) {
-        return res.status(400).send({error: 'SessionId does not have an user associated with it'})
+        return res.status(400).send({ error: 'SessionId does not have an user associated with it' })
     }
     // Check that the user is an admin
     if (!user.isAdmin) {
-        return res.status(400).send({error: 'Insufficient permissions'})
+        return res.status(400).send({ error: 'Insufficient permissions' })
     }
     next()
 }
-Array.prototype.findById = function (value) {
+Array.prototype.findById = function(value) {
     return this.findBy('id', parseInt(value))
 }
-Array.prototype.findBy = function (field, value) {
-    return this.find(function (x) {
-    }
-]
+Array.prototype.findBy = function(field, value) {
+    return this.find(function(x) {
+        return x[field] === value;
+    })
+}
+
 
 const temp_scores = [
     { text: "Enne töö, siis lõbu.", student: "Kevin Hartman" },
@@ -68,47 +64,44 @@ const temp_scores = [
 
 app.post('/users', (req, res) => {
     if (!req.body.name || !req.body.password || !req.body.personalId || !req.body.email || !req.body.school) {
-        return res.status(400).send({error: 'One or multiple params are missing'})
+        return res.status(400).send({ error: 'One or multiple params are missing' })
     }
     let personalId = users.findBy('personalId', req.body.personalId);
     if (personalId) {
-        return res.status(409).send({error: 'Conflict: The personal Id already exists, you have already made an account with this id. Contact support if you think you did not create an account. '})
+        return res.status(409).send({ error: 'Conflict: The personal Id already exists, you have already made an account with this id. Contact support if you think you did not create an account. ' })
     }
     let email = users.findBy('email', req.body.email);
     if (email) {
-        return res.status(409).send({error: 'Conflict: The email already exists. Press Forgot password to send account details to the email.'})
+        return res.status(409).send({ error: 'Conflict: The email already exists. Press Forgot password to send account details to the email.' })
     }
-    users.push({personalId: req.body.personalId, name: req.body.name, password: req.body.password, isAdmin: false, email: req.body.email, school: req.body.school})
+    users.push({ personalId: req.body.personalId, name: req.body.name, password: req.body.password, isAdmin: false, email: req.body.email, school: req.body.school })
     user = users.findBy('personalId', req.body.personalId);
     let newSession = {
         userId: user.personalId,
         id: req.body.email
     }
     sessions.push(newSession)
-    res.status(201).send({sessionId: sessions.findBy('id', req.body.email).userId })
+    res.status(201).send({ sessionId: sessions.findBy('id', req.body.email).userId })
 })
 app.post('/sessions', (req, res) => {
     if (!req.body.email || !req.body.password) {
-        return res.status(400).send({error: 'One or all params are missing'})
+        return res.status(400).send({ error: 'One or all params are missing' })
     }
     const user = users.find((user) => user.email === req.body.email && user.password === req.body.password);
     if (!user) {
-        return res.status(401).send({error: 'Unauthorized: email or password is incorrect'})
+        return res.status(401).send({ error: 'Unauthorized: email or password is incorrect' })
     }
     let newSession = {
         userId: user.personalId,
         id: req.body.email
     }
     sessions.push(newSession)
-    res.status(201).send(
-        {sessionId: sessions.findBy('id', req.body.email).userId}
-    )
+    res.status(201).send({ sessionId: sessions.findBy('id', req.body.email).userId })
 })
 app.delete('/sessions', (req, res) => {
     sessions = sessions.filter((session) => session.userId === req.body.sessionId);
     res.status(200).end()
 })
-// bodyParser Use
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -154,7 +147,7 @@ app.post('/API/ADMIN/STUDENTS', requireAdmin, (req, res) => {
     res.send(temp_admin)
 })
 app.post('/API/ADMIN/CHECK', requireAdmin, (req, res) => {
-    res.send({admin: true})
+    res.send({ admin: true })
 })
 app.delete('/API/ADMIN/STUDENTS', requireAdmin, (req, res) => {
     users = users.filter((user) => user.name !== req.body.name);
